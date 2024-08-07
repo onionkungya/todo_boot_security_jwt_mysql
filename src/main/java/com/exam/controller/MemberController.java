6984da4f02176ebfe092a6c26bb10dfabd2eec37
ponion <qkrwndjs9898@gmail.com>
2024-08-07 13:01:40 +0900

    third

--- src/main/java/com/exam/controller/MemberController.java
+++ src/main/java/com/exam/controller/MemberController.java
@@ -32,7 +32,7 @@ public class MemberController {
 	@GetMapping("/")
 	public String main( ) {
 		
-		return "main home- SCM Poll 실습.";
+		return "main home- SCM Poll 실습. 바보";
 	}
 	
 	@GetMapping("/hello")
